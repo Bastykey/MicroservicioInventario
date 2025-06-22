49300a57860d1ef5b0822606cfb3fc5a03bbf82e
Bastykey <169182698+Bastykey@users.noreply.github.com>
2025-06-22 09:47:08 -0400

    Delete MicroservicioInventario directory
    Tuve que eliminar este repositorio ya que subi el Microservicio y se duplico. Este ya no lo necesito.

--- MicroservicioInventario/src/main/java/cl/duoc/MicroservicioInventario/Repository/ProductoRepositoy.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.MicroservicioInventario.Repository;
-
-import cl.duoc.MicroservicioInventario.Model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepositoy  extends JpaRepository<Producto, Long> {
-}
